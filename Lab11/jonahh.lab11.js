1cfa9aea01f6081e2af59ef3807b47ca9801a31e
Jonah000 <jonahhiggins6@gmail.com>
2023-11-26 20:49:29 -0600

    Framework added

--- Lab11/jonahh.lab11.js
+++ Lab11/jonahh.lab11.js
@@ -1,10 +1,12 @@
-const api_url = "https://www.googleapis.com/books/v1"
+const api_url = "https://www.googleapis.com/books/v1";
+const library = [];
+const result = "";
 function Search() {
-
+    result = "";
 }
 function Add() {
-
+    library.add(result);
 }
 function Remove() {
-
+    library.remove(result);
 }
